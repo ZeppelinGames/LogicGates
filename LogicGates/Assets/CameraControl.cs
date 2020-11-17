269ef51e267dd9256dbbbb3e17aecc79ce122a64
Zeppelin Games <zeppelingames3@gmail.com>
2020-11-17 13:45:38 +1100

    Added camera zoom

--- LogicGates/Assets/CameraControl.cs
+++ LogicGates/Assets/CameraControl.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraControl : MonoBehaviour
 {
+    public float zoomSpeed=5;
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cam.orthographicSize += Input.mouseScrollDelta.y * zoomSpeed;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 1, 50);
     }
 }
